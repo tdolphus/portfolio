d53f8ccd66dc8cc2a0c2d12b930d99f0ae6b54aa
Titus <tdolphus@uncc.edu>
2023-07-12 19:19:55 -0700

    added scripts

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
