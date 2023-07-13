c97afc96d7532c0dbeb9fd12f61ceedbdbc999ce
Titus <tdolphus@uncc.edu>
2023-07-12 21:31:02 -0700

    redirect to ssl

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ const express = require('express');
 const app = express();
 
 
+
 app.listen(process.env.PORT || 3000);
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+app.use(function(request, response, next) {
+
+  if (process.env.NODE_ENV != 'development' && !request.secure) {
+     return response.redirect("https://" + request.headers.host + request.url);
+  }
+
+  next();
+})
 
 app.get("/", (req, res) => {
     res.render("index")
