ed2fc31b78d57efd745939b03d051bd04a2deb77
Titus <tdolphus@uncc.edu>
2023-07-16 20:51:37 -0700

    readding redirects

--- app.js
+++ app.js
@@ -10,20 +10,20 @@ app.set("trust proxy", true);
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-// app.use(function (request, response, next) {
-//   if (process.env.NODE_ENV != "development" && !request.secure) {
-//     return response.redirect("https://" + request.headers.host + request.url);
-//   }
-//   next();
-// });
+app.use(function (request, response, next) {
+  if (process.env.NODE_ENV != "development" && !request.secure) {
+    return response.redirect("https://" + request.headers.host + request.url);
+  }
+  next();
+});
 
-// app.use(function (req, res, next) {
-//   if (req.headers.host.slice(0, 4) === "www.") {
-//     var newHost = req.headers.host.slice(4);
-//     return res.redirect(301, req.protocol + "://" + newHost + req.originalUrl);
-//   }
-//   next();
-// });
+app.use(function (req, res, next) {
+  if (req.headers.host.slice(0, 4) === "www.") {
+    var newHost = req.headers.host.slice(4);
+    return res.redirect(301, req.protocol + "://" + newHost + req.originalUrl);
+  }
+  next();
+});
 
 app.get("/", (req, res) => {
   res.render("index");
