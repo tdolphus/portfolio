7a609577bc3908d6df77796112d91ab48a2964cb
Titus <tdolphus@uncc.edu>
2023-07-16 17:20:46 -0700

    change middleware order

--- app.js
+++ app.js
@@ -15,17 +15,18 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-app.use(function(request, response, next) {
 
-  if (process.env.NODE_ENV != 'development' && !request.secure) {
-     return response.redirect("https://" + request.headers.host + request.url);
-  }
-
-  next();
-})
 
 app.get("/", (req, res) => {
     res.render("index")
   });
 
+  app.use(function(request, response, next) {
+
+    if (process.env.NODE_ENV != 'development' && !request.secure) {
+       return response.redirect("https://" + request.headers.host + request.url);
+    }
+  
+    next();
+  })
   
